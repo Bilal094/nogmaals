c9f471e7aa3e40eaba4c9227ae02146d200a4785
Bilal094 <99066318@mydavinci.nl>
2021-10-13 09:45:44 +0200

    robotarm 9 gemaakt

---
+++ robotarm-python-2021-main/exercise9.py
@@ -0,0 +1,15 @@
+from RobotArm import RobotArm
+robotArm = RobotArm('exercise 9')
+robotArm.speed = (3)
+# Jouw python instructies zet je vanaf hier:
+for x in range(1,5):
+    for s in range(x):
+        robotArm.grab()
+        for i in range(5):
+            robotArm.moveRight()
+        robotArm.drop()
+        for a in range(5):
+            robotArm.moveLeft()
+    robotArm.moveRight()
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
